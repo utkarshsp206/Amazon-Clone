12c62821aed7dd607068a8bbc0078f0ce979b6a7
utkarhsp206 <utkarhsp206@gmail.com>
2023-08-15 03:54:55 +0530

    Commit after first video

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbaar from './components/header/Navbaar';
+import Maincomp from './components/home/Maincomp';
+import Newnav from './components/newnavbaar/Newnav'
 
 
 function App() {
   return (
     <>
     <Navbaar/>
+    <Newnav/>
+    <Maincomp/>
     </>
   );
 }
---
+++ client/src/components/home/Maincomp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './Banner.js'
+import './home.css'
+
+function Maincomp() {
+  return (
+    <div className="home_section">
+        <div className="banner_part">
+            <Banner/>
+        </div>
+    </div>
+  )
+}
+
+export default Maincomp
